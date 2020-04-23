5fb51dd032dd0342130268e9199e225c00204223
Temitope-Emmanuel <temitopeojo0@gmail.com>
2020-04-23 01:41:33 +0100

    deploy to netlify

---
+++ build/precache-manifest.51d227c5f573562e0d07933ad90624a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9159bc0127703faa20b",
+    "url": "/static/css/main.85259da2.chunk.css"
+  },
+  {
+    "revision": "f9159bc0127703faa20b",
+    "url": "/static/js/main.7dd9035a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0580ac016c1c939bba6d",
+    "url": "/static/css/2.2adee338.chunk.css"
+  },
+  {
+    "revision": "0580ac016c1c939bba6d",
+    "url": "/static/js/2.81b4575c.chunk.js"
+  },
+  {
+    "revision": "b490249638d5642666f23805a0b83e6c",
+    "url": "/static/media/bg.b4902496.svg"
+  },
+  {
+    "revision": "2db2835b9048a2cb737bb769c559a1b6",
+    "url": "/index.html"
+  }
+];
