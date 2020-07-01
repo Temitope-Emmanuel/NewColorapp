4d1405eb8f092849fc8afa4293c1a1c26725b356
Temitope-Emmanuel <temitopeojo0@gmail.com>
2020-07-01 01:57:19 +0100

    added the redirects

---
+++ build/precache-manifest.06ac699eb9bab5b517d60f5ca0293665.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3ebf50a6f6d2e1ed98e6",
+    "url": "/static/css/main.85259da2.chunk.css"
+  },
+  {
+    "revision": "3ebf50a6f6d2e1ed98e6",
+    "url": "/static/js/main.f63713f9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0580ac016c1c939bba6d",
+    "url": "/static/css/2.2adee338.chunk.css"
+  },
+  {
+    "revision": "0580ac016c1c939bba6d",
+    "url": "/static/js/2.81b4575c.chunk.js"
+  },
+  {
+    "revision": "b490249638d5642666f23805a0b83e6c",
+    "url": "/static/media/bg.b4902496.svg"
+  },
+  {
+    "revision": "c76bf2d753cae6aea501a33f068aa52c",
+    "url": "/index.html"
+  }
+];
